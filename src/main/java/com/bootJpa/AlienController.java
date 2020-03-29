e9b2aed2265aa36c1b511844ab037528f88bd232
EkanayakeNP <ekanayake65np@gmail.com>
2020-03-29 17:43:39 +0530

    added comment

--- src/main/java/com/bootJpa/AlienController.java
+++ src/main/java/com/bootJpa/AlienController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AlienController {
 
+	//commented by np
 	@Autowired
 	private TestService testService;
 
